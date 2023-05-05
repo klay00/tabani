6706b02eebf0ade7660b4be299d65baf593139e3
klay00 <aali74432@gmail.com>
2023-05-05 11:37:40 +0300

    add db fiirestore

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwCpkd5iPljS952Txf6XyFEXOgRDIdXCI",
+  authDomain: "tabani-92024.firebaseapp.com",
+  projectId: "tabani-92024",
+  storageBucket: "tabani-92024.appspot.com",
+  messagingSenderId: "847731413659",
+  appId: "1:847731413659:web:67092c41dad9d548eeac3e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app);
