2c65d249746d00bfe13621f77307feb4fccdd48a
klay00 <aali74432@gmail.com>
2023-05-06 23:44:17 +0300

    add router to app.js to routing pages

--- src/App.js
+++ src/App.js
@@ -1,75 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { db } from './firebase/firebase.js';
-import { useEffect, useState } from 'react';
-import { collection, onSnapshot, query, doc,addDoc, deleteDoc } from "firebase/firestore";
+import AllProduct from './pages/allproduct';
+import HomePage from "./pages/homepage";
+import LogIn from './pages/login';
+import NoPage from './pages/nopage';
+import PetPage from './pages/petpage';
+import SignUp from './pages/signup';
+import Profile from './pages/profile';
 
 
 function App() {
-  const [user, setuser] = useState([]);
-  const [name, setname] = useState('');
-  const [mail, setmail] = useState('');
-
-
-
-  useEffect(() => {
-    //read data form databast
-    const q = query(collection(db, 'users'))
-    onSnapshot(q, (querySnapshot) => {
-      setuser(
-        querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }))
-      );
-    })
-
-  }, [])
-
-  //add to data to database value of input
-  function handelsubmit(e) {
-    e.preventDefault();
-    const docRef = addDoc(collection(db, "users"), {
-      name,
-      mail
-    });  
-  } 
-
-  //delete data from data base
-  function handeldelete(id) {
-
-
-      const deleteuser = doc(db, 'users',id);
-
-     deleteDoc(deleteuser).then(()=>{
-      console.log(`the user ${id} is deletied`);
-     })
-    }
-
+   
   return (
-    <div className="App">
-      <form onSubmit={handelsubmit}>
-        <input type={'text'} placeholder="name" value={name} onChange={((e) => setname(e.target.value))} />
-        <input type={'text'} placeholder="mail" value={mail} onChange={((e) => setmail(e.target.value))} />
-        <input type={'submit'} placeholder="submit" />
-
-      </form>
-      <>
-        {
-          user.map((user) => {
-            return (
-              <>
-                <div>{user.name}</div>
-                <div >{user.mail}</div>
-                <button onClick={()=>(handeldelete(user.id))}>delete</button>
-
-              </>
-
-            )
-          })
-        }
-      </>
-
-    </div>
+    <>
+     <BrowserRouter>
+     <Routes path="./pages/">
+     <Route index element={<HomePage/>}/>
+     <Route  path='/login' element={<LogIn/>}/>
+     <Route  path='/signup' element={<SignUp/>}/>
+     <Route  path='/allproduct' element={<AllProduct/>}/>
+     <Route  path='/profile' element={<Profile/>}/>
+     <Route  path='/petpage' element={<PetPage/>}/>
+     <Route  path='*' element={<NoPage/>}/>
+     </Routes>
+     
+     </BrowserRouter>
+    </>
   );
 }
 
