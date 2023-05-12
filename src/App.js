8fa45010d13dd91e3a9b8cd49d137a5217b3cb35
klay00 <aali74432@gmail.com>
2023-05-12 19:40:46 +0300

    import and using pagelist

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import AllProduct from './pages/allproduct';
 import HomePage from "./pages/homepage";
 import LogIn from './pages/login';
-import NoPage from './pages/nopage';
-import PetPage from './pages/petpage';
 import SignUp from './pages/signup';
-import Profile from './pages/profile';
-import NavBar from './components/navbar';
+import pagelist from './components/pagelist';
+
 
 
 function App() {
-  const pagelist = [
-    {
-      path: '/allproduct',
-      page: <AllProduct />
-    },
-    {
-      path: '/profile',
-      page: <Profile />
-    },
-    {
-      path: '/petpage',
-      page: <PetPage />
-    }
-    ,
-    {
-      path: '*',
-      page: <NoPage />
-    }
-  ];
 
   return (
     <>
