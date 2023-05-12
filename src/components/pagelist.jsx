60da6f7cf927c258efd26fa6570c72a64ea02676
klay00 <aali74432@gmail.com>
2023-05-12 20:28:11 +0300

    create nav list to routing bettwen pages with nav

--- src/components/pagelist.jsx
+++ src/components/pagelist.jsx
@@ -1,28 +1,50 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AllProduct from '../pages/allproduct';
 import NoPage from '../pages/nopage';
 import PetPage from '../pages/petpage';
 import Profile from '../pages/profile';
-import '../App.css';
+import HomePage from '../pages/homepage';
 
- const pagelist = [
-    {
-      path: '/allproduct',
-      page: <AllProduct />
-    },
-    {
-      path: '/profile',
-      page: <Profile />
-    },
-    {
-      path: '/petpage',
-      page: <PetPage />
-    }
-    ,
-    {
-      path: '*',
-      page: <NoPage />
-    }
-  ];  
+const pagelist = [
+  {
+    path: '/allproduct',
+    page: <AllProduct />
+  },
+  {
+    path: '/profile',
+    page: <Profile />
+  },
+  {
+    path: '/petpage',
+    page: <PetPage />
+  }
+  ,
+  {
+    path: '*',
+    page: <NoPage />
+  }
+];
+
+export const NavPages = [
+  {
+    name:'Blog',
+    path: '/',
+    page: <HomePage />
+  },
+  {
+    name:'All Pets',
+    path: '/allproduct',
+    page: <AllProduct />
+  },
+  {
+    name:'Cats',
+    path: '/allproduct',
+    page: <AllProduct />
+  },
+  {
+    name:'Dogs',
+    path: '/allproduct',
+    page: <AllProduct />
+  },
+];
 export default pagelist;
