77acba83b9be91e0732b254cc82d67863a8f1064
klay00 <aali74432@gmail.com>
2023-05-14 18:58:08 +0300

    custmaise theem in the project

---
+++ src/tools/theem.jsx
@@ -0,0 +1,12 @@
+import {  createTheme } from "@mui/material/styles";
+ const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#FFA800"
+      },
+      secondary: {
+        main: "#FFA800"
+      }
+    }
+  });
+  export default theme;
