0ef665b9076c9c97685b3eb1d4ed86ea6d773159
klay00 <aali74432@gmail.com>
2023-05-14 18:59:38 +0300

    add  search componant in the main display

--- src/components/main_display.jsx
+++ src/components/main_display.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../App.css';
 import OverflowCard from './cart';
+import Search from './serch';
 
 export default function MainDisplay() {
     return(
         <>
         <div className="main">
-            <div className="first"></div>
+            <div className="first">
+                <Search/>
+            </div>
             <div className="secnde"></div>
             <div className="main-display-items">
             <OverflowCard/>
