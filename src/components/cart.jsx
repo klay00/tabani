bc1f493886938729219959c4c6fa3943717e13bd
klay00 <aali74432@gmail.com>
2023-05-17 22:40:53 +0300

    add link to naviget to pages profile and pet page

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -4,26 +4,33 @@ import Card from '@mui/joy/Card';
 import CardOverflow from '@mui/joy/CardOverflow';
 import Divider from '@mui/joy/Divider';
 import Typography from '@mui/joy/Typography';
+import { Link } from "react-router-dom";
 
 export default function OverflowCard() {
   return (
-    
-    <Card variant="outlined" sx={{ width: 250 ,py: 0,mb:2}}>
-      <CardOverflow>
+    <Card variant="outlined" sx={{ width: 250, py: 0, mb: 2 }}>
+      <CardOverflow fullWidth >
         <AspectRatio ratio="1.5">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzuw3ZNTNZZQgARGpWW7f6hColBKQKZ9qo6eW3giZIawfCbFziSINYfpht19iH8ndNnQA&usqp=CAU"
-            // srcSet="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x"
-            loading="lazy"
-            alt=""
-          />
+          <Link to={'../petpage'}>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzuw3ZNTNZZQgARGpWW7f6hColBKQKZ9qo6eW3giZIawfCbFziSINYfpht19iH8ndNnQA&usqp=CAU"
+              // srcSet="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x"
+              loading="lazy"
+              alt=""
+            />
+          </Link>
         </AspectRatio>
       </CardOverflow>
       <Typography level="h2" sx={{ fontSize: 'md', mt: 1 }}>
-        Yosemite National Park 
+        <Link to={'../petpage'}>
+          Yosemite National Park
+        </Link>
+
       </Typography>
       <Typography level="body2" sx={{ mt: 0.5, mb: 1 }}>
-        California
+        <Link to={'../profile'}>
+          California
+        </Link>
       </Typography>
       <Divider />
       <CardOverflow
