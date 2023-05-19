12c5bd51629eb6e96b446a9dbeb4e0823989fe33
klay00 <aali74432@gmail.com>
2023-05-19 14:56:46 +0300

    add table information pet

--- src/pages/petpage.jsx
+++ src/pages/petpage.jsx
@@ -3,22 +3,39 @@ import NavBar from "../components/navbar";
 import '../App.css';
 import { AspectRatio, Button } from "@mui/joy";
 import { catImageList } from "../components/pagelist";
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import MainButtom from "../components/buttom";
 
 export default function PetPage() {
-    const [imgUrl,setImgUrl]=useState([]);
+
+    //   const rows = [
+    //     createData('Type', "Dog"),
+    //     createData('Color', 'white'),
+    //     createData('Age', '2 yers'),
+    //     createData('Sex', 'male'),
+    //     createData('Location', 'krada'),
+    //     createData('Size', '5kg'),
+    //     createData('Vaccination from the disease', 'yes'),
+    //   ];
+
+
+    const [imgUrl, setImgUrl] = useState([]);
     // setImgUrl(0);
-    useEffect(()=>{
-        catImageList.map((img)=>{
-            // console.log(img[0]);
+    useEffect(() => {
+        catImageList.map((img) => {
             setImgUrl(img.imgSrc)
-        }) 
-    },[]);
-    function handellUrl(url) {       
-            console.log(url)
-            setImgUrl(url)                                   
+        })
+    }, []);
+    function handellUrl(url) {
+        setImgUrl(url)
     }
-   
+
     return (
         <>
             <NavBar />
@@ -33,26 +50,78 @@ export default function PetPage() {
                             />
                         </AspectRatio>
                     </div>
-                    <div className="chose-image">                      
-                             {
-                                catImageList.map((catimg)=>{
-                                    return(
-                                        <>
-                                        <Button onClick={()=>{handellUrl(catimg.imgSrc)}}>
-                                    <img
-                                        src={catimg.imgSrc}
-                                        loading="lazy"
-                                        alt="pet image"
-                                    />
-                            </Button>
-                                        </>
-                                    )
-                                })
-                             }
+                    <div className="chose-image">
+                        {
+                            catImageList.map((catimg) => {
+                                return (
+                                    <>
+                                        <Button onClick={() => { handellUrl(catimg.imgSrc) }}>
+                                            <img
+                                                src={catimg.imgSrc}
+                                                loading="lazy"
+                                                alt="pet image"
+                                            />
+                                        </Button>
+                                    </>
+                                )
+                            })
+                        }
                     </div>
                 </div>
                 <div className="pet-info">
-                    <div className="info">hghghgh</div>
+                    <div className="info">
+                        <table className="pet-info-table">
+                            <thead>
+                                <tr className="pet-info-head">
+                                    <td ><h3>pet name</h3> </td>
+                                    <td className="avilple-to-adopt">Avilple to adubt</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr >
+                                    <td ><h4>Type</h4> </td>
+                                    <td>Dog</td>
+
+                                </tr>
+                                <tr>
+                                    <td ><h4>Color</h4> </td>
+                                    <td>White</td>
+
+                                </tr>
+                                <tr>
+                                <td ><h4>Age</h4> </td>
+                                <td>2 yers</td>
+
+                                    
+                                </tr>
+                                <tr>
+                                <td ><h4>Sex</h4> </td>
+                                <td>Male</td>
+
+                                    
+                                </tr>
+                                <tr>
+                                <td ><h4>Location</h4> </td>
+                                <td>Dora</td>
+
+                                    
+                                </tr>
+                                <tr>
+                                <td ><h4>Size</h4> </td>
+                                <td>5 kg</td>
+
+                                </tr>
+                                <tr>
+                                <td ><h4>Vaccination from the disease</h4></td>
+                                <td>yes</td>
+
+                                </tr>
+                                <tr className="but-adobt">
+                                    <MainButtom name={'Adobt Now'}/>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                     <div className="account">ffffffffff</div>
                 </div>
             </div>
