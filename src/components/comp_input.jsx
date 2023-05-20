d7161b860bf7d9f937138a09814ae70fa13113c1
klay00 <aali74432@gmail.com>
2023-05-20 17:52:21 +0300

    add componant comp formik and yup

--- src/components/comp_input.jsx
+++ src/components/comp_input.jsx
@@ -1,68 +1,97 @@
 import React from 'react';
-import { Formik } from 'formik';
+import '../App.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  fullName: Yup.string().required('Full Name is required'),
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  address: Yup.string().required('Address is required'),
+  phoneNumber: Yup.string().required('Phone Number is required'),
+  placement: Yup.string().required('Placement is required'),
+  care: Yup.string().required('Care is required'),
+});
+
+const onSubmit = (values) => {
+  alert(JSON.stringify(values, null, 2));
+}; 
 
 export default function CompInput() {
   return (
-    <>
-      <div>
-        <h2>Apply to Adopt</h2>
-        <Formik
-          initialValues={{ email: '', password: '' }}
-          validate={values => {
-            const errors = {};
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (
-              console.log('ffffffffffffff'),
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address';
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <input
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-              {errors.email && touched.email && errors.email}
-              <input
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-              />
-              {errors.password && touched.password && errors.password}
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </form>
-          )}
-        </Formik>
-      </div>
 
-    </>
+       
+  
+
+
+  <div>
+      <h2>Apply to Adopt</h2>
+      <Formik
+        initialValues={{
+          fullName: '',
+          email: '',
+          address: '',
+          phoneNumber: '',
+          placement: '',
+          care: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form className='formik-dispy'>
+
+          <div className='form-inputs'>
+
+          
+        <div className='input-lp'>
+                   
+             <label htmlFor="fullName">Full Name</label>
+            <Field type="text" id="fullName" name="fullName" placeholder="Enter your full name" />
+            <ErrorMessage name="fullName" component="div" />
+                </div>
+    
+
+          <div className='input-lp'>
+            <label htmlFor="email">Email</label>
+            <Field type="email" id="email" name="email" placeholder="Enter your email address" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+
+          <div  className='input-lp'>
+            <label htmlFor="address">Address</label>
+            <Field type="text" id="address" name="address" placeholder="Enter your address" />
+            <ErrorMessage name="address" component="div" />
+          </div>
+
+          <div  className='input-lp'>
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <Field type="text" id="phoneNumber" name="phoneNumber" placeholder="Enter your phone number"/>
+            <ErrorMessage name="phoneNumber" component="div" />
+          </div>
+
+          <div  className='input-lp'>
+            <label htmlFor="placement">Do you have a suitable place for the pet to stay?</label>
+            <Field type="text" id="placement" name="placement" placeholder="Describe where the pet lives"/>
+            <ErrorMessage name="placement" component="div" />
+          </div>
+
+          <div  className='input-lp'>
+            <label htmlFor="care">Do you undertake to take care of the pet?</label>
+            <Field type="text" id="care" name="care"  placeholder="Describe the methods of care used that you know about caring for a pet"/>
+            <ErrorMessage name="care" component="div" />
+          </div>
+          
+           
+          
+            
+
+           
+          
+          
+          </div>
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
   )
 
 }
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -7,6 +7,7 @@ export default function Profile() {
         <>
         <NavBar/>
           profile page 
+          
         </>
     );
 }
