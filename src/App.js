b20e226c40896a3375245e1b5a3d62b6fca2230d
klay00 <aali74432@gmail.com>
2023-05-21 15:42:54 +0300

    routing user dash in app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import pagelist from './components/pagelist';
+import DashUser from './pages/dash_user';
 import HomePage from "./pages/homepage";
 import LogIn from './pages/login';
 import SignUp from './pages/signup';
@@ -27,6 +28,7 @@ function App() {
           <Route >
             <Route path='/login' element={<LogIn />} />
             <Route path='/signup' element={<SignUp />} />
+            <Route path='/dash_user' element={<DashUser/>} />
           </Route>
         </Routes>
 
