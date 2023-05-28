8e5966e469a0c7be88b1ad4f7b404cf8a0f42024
klay00 <aali74432@gmail.com>
2023-05-29 01:40:24 +0300

    view user data and pets that he add

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,11 +1,37 @@
 import { Avatar } from "@mui/material";
 import { Stack } from "@mui/system";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import OverflowCard from "../components/cart";
 import NavBar from "../components/navbar";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { useLocation } from "react-router-dom";
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+import { SkeltonCard } from "../components/skelton_card";
 
 export default function Profile() {
+  const data=useLocation();
+  const userData=data.state;
+  console.log(userData);
+const [loding ,setLoding]=useState(false)
+  const [petData, setPetData] = useState([]);
+  const featchUserData = async () => {
+    setLoding(true);
+      const q = await getDocs(collection(db, 'pets'));
+      const petData = q.docs.filter((doc) => doc.data().userId === userData.userId)
+          .map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+          }));
+          setPetData(petData)
+          setLoding(false);
+
+  }
+useEffect(()=>{
+  
+  featchUserData();
+},[])
+console.log(loding);
   return (
     <>
       <NavBar />
@@ -18,11 +44,11 @@ export default function Profile() {
             <Avatar sx={{ marginRight: '20px', width: 65, height: 65 }} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzuw3ZNTNZZQgARGpWW7f6hColBKQKZ9qo6eW3giZIawfCbFziSINYfpht19iH8ndNnQA&usqp=CAU'>
             </Avatar>
             <div>
-              <h2 >the account name</h2>
+              <h2 >{userData.fullName}</h2>
               <Stack direction={{ xs: 'row', sm: 'row' }}
                 spacing="2"
               >
-                <LocationOnIcon></LocationOnIcon> Dora
+                <LocationOnIcon></LocationOnIcon>{userData.location}
               </Stack>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae a odit ea porro mollitia, aliquid, fugit sint distinctio consectetur quia voluptate facere exercitationem adipisci quidem aut in ut sed quasi!</p>
 
@@ -31,14 +57,34 @@ export default function Profile() {
           </Stack>
         </div>
         <div className="main-display-items">
-          <OverflowCard />
-          <OverflowCard />
-          <OverflowCard />
-          <OverflowCard />
-          <OverflowCard />
-          <OverflowCard />
-          <OverflowCard />
-          <OverflowCard />
+          
+
+           {
+            petData.map((pet)=>{
+              return(
+                <>
+                {
+                  !loding?                 
+                  <OverflowCard
+                  key={pet.id} // Add a unique key for each card
+                  petname={pet.fullName}
+                  userfullName={userData.fullName}
+                  petImage={pet.images[0]}
+                  userLocation={userData.location}
+                  petState={pet.status}
+                  petId={pet.id}
+                  pet={pet}
+                  user={userData}
+                /> :<SkeltonCard/>
+                 
+                }
+                 
+                </>
+              )
+            })
+           }
+
+          
         </div>
       </div>
     </>
