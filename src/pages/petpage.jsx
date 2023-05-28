0632dd61e2580a4b777dab6e84c0717d55edb6f2
klay00 <aali74432@gmail.com>
2023-05-28 18:42:44 +0300

    display information pet

--- src/pages/petpage.jsx
+++ src/pages/petpage.jsx
@@ -4,19 +4,26 @@ import '../App.css';
 import { AspectRatio, Avatar, Button, Stack } from "@mui/joy";
 import { catImageList } from "../components/pagelist";
 import MainButtom from "../components/buttom";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import FullScreenDialog from "../components/adubt_inputs";
+import { collection, getDocs, onSnapshot, query } from 'firebase/firestore';
+import '../App.css';
+import { db } from '../firebase/firebase';
+
+
+
 
 export default function PetPage() {
 
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
-      setOpen(true);
+        setOpen(true);
     };
     const handleClose = () => {
-      setOpen(false);
+        setOpen(false);
     };
+
 
     const [imgUrl, setImgUrl] = useState([]);
     // setImgUrl(0);
@@ -28,7 +35,12 @@ export default function PetPage() {
     function handellUrl(url) {
         setImgUrl(url)
     }
-
+    const location = useLocation();
+    const [pet, setpet] = useState([]);
+    useEffect(() => {
+        setpet(location.state);
+    }, []);
+    console.log(pet);
     return (
         <>
             <NavBar />
@@ -51,19 +63,34 @@ export default function PetPage() {
                     </div>
                     <div className="chose-image">
                         {
-                            catImageList.map((catimg) => {
-                                return (
-                                    <>
-                                        <Button onClick={() => { handellUrl(catimg.imgSrc) }}>
-                                            <img
-                                                src={catimg.imgSrc}
-                                                loading="lazy"
-                                                alt="pet image"
-                                            />
-                                        </Button>
-                                    </>
-                                )
-                            })
+
+
+                            // pet.map((petObj, index) => (
+                            //     <div key={index}>
+                            //         {petObj.images.map((imageUrl, imageIndex) => (
+                            //             <Button onClick={() => { handellUrl(imageUrl[imageIndex]) }}>
+                            //                 <img
+                            //                     src={imageUrl[imageIndex]}
+                            //                     loading="lazy"
+                            //                     alt="pet image"
+                            //                 />
+                            //             </Button>
+                            //         ))}
+                            //     </div>
+                            // ))
+
+                            //         return (
+                            //             <>
+                            //                 <Button onClick={() => { handellUrl(catimg[i]) }}>
+                            //                     <img
+                            //                         src={catimg[i]}
+                            //                         loading="lazy"
+                            //                         alt="pet image"
+                            //                     />
+                            //                 </Button>
+                            //             </>
+                            //         )
+                            //     })
                         }
                     </div>
                 </div>
@@ -72,47 +99,42 @@ export default function PetPage() {
                         <table className="pet-info-table">
                             <thead>
                                 <tr className="pet-info-head">
-                                    <td ><h3>pet name</h3> </td>
-                                    <td className="avilple-to-adopt">Avilple to adubt</td>
+                                    <td ><h3>{pet.fullName}</h3> </td>
+                                    <td className="avilple-to-adopt">{pet.status}</td>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr >
                                     <td ><h4>Type</h4> </td>
-                                    <td>Dog</td>
-
-                                </tr>
-                                <tr>
-                                    <td ><h4>Color</h4> </td>
-                                    <td>White</td>
+                                    <td>{pet.type}</td>
 
                                 </tr>
                                 <tr>
                                     <td ><h4>Age</h4> </td>
-                                    <td>2 yers</td>
+                                    <td>{pet.age}</td>
 
 
                                 </tr>
                                 <tr>
                                     <td ><h4>Sex</h4> </td>
-                                    <td>Male</td>
+                                    <td>{pet.sex}</td>
 
 
                                 </tr>
                                 <tr>
                                     <td ><h4>Location</h4> </td>
-                                    <td>Dora</td>
+                                    <td>{pet.status}</td>
 
 
                                 </tr>
                                 <tr>
                                     <td ><h4>Size</h4> </td>
-                                    <td>5 kg</td>
+                                    <td>{pet.size}</td>
 
                                 </tr>
                                 <tr>
                                     <td ><h4>Vaccination from the disease</h4></td>
-                                    <td>yes</td>
+                                    <td>{pet.avcciation}</td>
 
                                 </tr>
 
@@ -122,7 +144,7 @@ export default function PetPage() {
 
                         </table>
                         <div className="but-adobt">
-                            <FullScreenDialog/>
+                            <FullScreenDialog />
                         </div>
                     </div>
                     <div className="account">
@@ -130,13 +152,13 @@ export default function PetPage() {
                             direction={{ xs: 'row', sm: 'row' }}
                             spacing="2"
                             alignItems="center">
-                            <Avatar  sx={{ marginRight: '20px' ,width: 65, height: 65 }} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzuw3ZNTNZZQgARGpWW7f6hColBKQKZ9qo6eW3giZIawfCbFziSINYfpht19iH8ndNnQA&usqp=CAU'>
+                            <Avatar sx={{ marginRight: '20px', width: 65, height: 65 }} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzuw3ZNTNZZQgARGpWW7f6hColBKQKZ9qo6eW3giZIawfCbFziSINYfpht19iH8ndNnQA&usqp=CAU'>
                             </Avatar>
                             <h2 >the account name</h2>
                         </Stack>
                         <div className="but-adobt">
                             <Link to={'/profile'}>
-                            <MainButtom name={'view Account'} />
+                                <MainButtom name={'view Account'} />
                             </Link>
                         </div>
                     </div>
