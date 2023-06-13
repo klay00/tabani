ef02178fba8797bc86b24f4e530638659c1113ec
klay00 <aali74432@gmail.com>
2023-06-13 22:21:43 +0300

    add the edit to profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -2,37 +2,76 @@ import { Avatar } from "@mui/material";
 import { Stack } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import OverflowCard from "../components/cart";
-import NavBar from "../components/navbar";
+import NavBar, { userInfo } from "../components/navbar";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { useLocation } from "react-router-dom";
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../firebase/firebase';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { auth, db } from '../firebase/firebase';
 import { SkeltonCard } from "../components/skelton_card";
-import { Button, IconButton } from "@mui/joy";
-import EditIcon from '@mui/icons-material/Edit';
+import EditProfile from "../components/EditProfile";
+
+
+
 export default function Profile() {
-  const data=useLocation();
-  const userData=data.state;
-  console.log(userData);
-const [loding ,setLoding]=useState(false)
+  const data = useLocation();
+  const userData = data.state;
+  // console.log(userData);
+  const [loding, setLoding] = useState(false)
   const [petData, setPetData] = useState([]);
   const featchUserData = async () => {
     setLoding(true);
-      const q = await getDocs(collection(db, 'pets'));
-      const petData = q.docs.filter((doc) => doc.data().userId === userData.userId)
-          .map((doc) => ({
-              id: doc.id,
-              ...doc.data(),
-          }));
-          setPetData(petData)
-          setLoding(false);
+    const q = await getDocs(collection(db, 'pets'));
+    const petData = q.docs.filter((doc) => doc.data().userId === userData.userId)
+      .map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+    setPetData(petData)
+    setLoding(false);
 
   }
-useEffect(()=>{
-  
-  featchUserData();
-},[])
-console.log(loding);
+  useEffect(() => {
+
+    featchUserData();
+  }, [])
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (authUser) => {
+      if (authUser) {
+        // User is logged in, fetch user information from Firestore
+        fetchUserData(authUser.uid);
+      } else {
+        // User is not logged in
+        setUser(null);
+      }
+    });
+
+    // Clean up the listener
+    return () => unsubscribe();
+  }, []);
+
+  const fetchUserData = async (userId) => {
+    try {
+      const userRef = doc(db, 'users', userId);
+      const snapshot = await getDoc(userRef);
+
+      if (snapshot.exists()) {
+        const userData = snapshot.data();
+        setUser(userData);
+
+      } else {
+        // User document does not exist
+        setUser(null);
+      }
+    } catch (error) {
+      console.log('Error fetching user data:', error);
+    }
+  };
+  console.log('-------------------------');
+  console.log(user)
+  console.log('-------------------------');
   return (
     <>
       <NavBar />
@@ -43,11 +82,11 @@ console.log(loding);
             spacing={2}
           >
             <Stack direction={'column'} spacing={1} >
-            <Avatar sx={{ marginRight: '20px', width: 65, height: 65 }} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzuw3ZNTNZZQgARGpWW7f6hColBKQKZ9qo6eW3giZIawfCbFziSINYfpht19iH8ndNnQA&usqp=CAU'>
-            </Avatar>
-            <IconButton color="warning" sx={{pl:1,pr:1,width:60}}>
-              <EditIcon/>
-            </IconButton>
+              <Avatar sx={{ marginRight: '20px', width: 65, height: 65 }} src={userData.profileImage}>
+              </Avatar>
+              {
+                user?.userId === userData?.userId ? <EditProfile userId={userData?.userId} /> : null
+              }
             </Stack>
             <div>
               <h2 >{userData.fullName}</h2>
@@ -56,41 +95,47 @@ console.log(loding);
               >
                 <LocationOnIcon></LocationOnIcon>{userData.location}
               </Stack>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae a odit ea porro mollitia, aliquid, fugit sint distinctio consectetur quia voluptate facere exercitationem adipisci quidem aut in ut sed quasi!</p>
+              <p>
+                {
+                  userData.discreption
+                 
+                }
+              </p>
 
             </div>
 
           </Stack>
         </div>
         <div className="main-display-items">
-          
 
-           {
-            petData.map((pet)=>{
-              return(
+
+          {
+            petData.map((pet) => {
+              return (
                 <>
-                {
-                  !loding?                 
-                  <OverflowCard
-                  key={pet.id} // Add a unique key for each card
-                  petname={pet.fullName}
-                  userfullName={userData.fullName}
-                  petImage={pet.images[0]}
-                  userLocation={userData.location}
-                  petState={pet.status}
-                  petId={pet.id}
-                  pet={pet}
-                  user={userData}
-                /> :<SkeltonCard/>
-                 
-                }
-                 
+                  {
+                    !loding ?
+                      <OverflowCard
+                        key={pet.id} // Add a unique key for each card
+                        petname={pet.fullName}
+                        userfullName={userData.fullName}
+                        petImage={pet.images[0]}
+                        userLocation={userData.location}
+                        petState={pet.status}
+                        petId={pet.id}
+                        pet={pet}
+                        user={userData}
+                        userImage={userData.profileImage}
+                      /> : <SkeltonCard />
+
+                  }
+
                 </>
               )
             })
-           }
+          }
 
-          
+
         </div>
       </div>
     </>
