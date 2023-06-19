0f32c9570bcff890538c9d8a5104833f5ebfb81d
klay00 <aali74432@gmail.com>
2023-06-20 00:34:16 +0300

    get length of data

--- src/admin/pages/AdminHome.jsx
+++ src/admin/pages/AdminHome.jsx
@@ -1,22 +1,116 @@
-import { Box, margin } from "@mui/system";
-import React from "react";
+import { Stack } from "@mui/joy";
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
 import SideBar from "../components/SideBar";
+import { auth, db } from '../../firebase/firebase';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+
 
 
 export default function AdminHome() {
-    return(
+    const [pet,setPet]=useState([]);
+    const [user,setUser]=useState([]);
+    const [order,setOrder]=useState([]);
+    const [loding,setLoding]=useState(false);
+
+    useEffect(() => {
+        setLoding(true)
+        fetchPetData();
+        fetchUserData();
+        fetchOrderData();
+        setLoding(false);
+      }, []);
+
+    const fetchPetData = async () => {
+        const petQuerySnapshot = await getDocs(collection(db, 'pets'));
+        const petData = petQuerySnapshot.docs.filter(doc=>doc.data() )
+        .map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPet(petData);
+      };
+      const fetchUserData = async () => {
+        const petQuerySnapshot = await getDocs(collection(db, 'users'));
+        const userdata = petQuerySnapshot.docs.filter(doc=>doc.data() )
+        .map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setUser(userdata);
+      };
+      const fetchOrderData = async () => {
+        const petQuerySnapshot = await getDocs(collection(db, 'order'));
+        const orderdata = petQuerySnapshot.docs.filter(doc=>doc.data() )
+        .map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setOrder(orderdata);
+      };
+    return (
         <>
-        <SideBar/>
-        <Box 
-        sx={{
-            marginLeft:10,
-            marginTop:5
-           
-        }}
-        >
-            
-            
-        </Box>
+            <SideBar />
+            <Box
+                sx={{
+                    marginLeft: 10,
+                    marginTop: 5,
+                    marginRight: 3,
+                }}
+            >
+
+                <Stack direction={'row'} sx={{ flexWrap: 'wrap', gap: 2 }}>
+                    <Box
+                        sx={{
+                            background: 'linear-gradient(45deg, #FF6B6B, #FF8E53)',
+                            width: 250,
+                            height: 200,
+                            borderRadius: 3,
+                            flexGrow: 1,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+                        }}
+                    >
+                        <h2 style={{ margin: 0, fontSize: '24px', color: '#FFF' }}>Users :{user.length}</h2>
+                    </Box>
+                    <Box
+                        sx={{
+                            background: 'linear-gradient(45deg, #8D6BF5, #BF9BFF)',
+                            width: 250,
+                            height: 200,
+                            borderRadius: 3,
+                            flexGrow: 1,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+                        }}
+                    >
+                        <h2 style={{ margin: 0, fontSize: '24px', color: '#FFF' }}>Pets :{pet.length}</h2>
+
+                    </Box>
+                    <Box
+                        sx={{
+                            background: 'linear-gradient(45deg, #6BD8FF, #53CFFF)',
+                            width: 250,
+                            height: 200,
+                            borderRadius: 3,
+                            flexGrow: 1,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+                        }}
+                    >
+                        <h2 style={{ margin: 0, fontSize: '24px', color: '#FFF' }}>Orders : {order.length}</h2>
+                    </Box>
+                </Stack>
+            </Box>
         </>
     )
 }
