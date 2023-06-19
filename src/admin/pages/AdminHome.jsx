4361513c7f662487edead17addd006a283cece7f
klay00 <aali74432@gmail.com>
2023-06-19 22:39:35 +0300

    create admin mian page

---
+++ src/admin/pages/AdminHome.jsx
@@ -0,0 +1,22 @@
+import { Box, margin } from "@mui/system";
+import React from "react";
+import SideBar from "../components/SideBar";
+
+
+export default function AdminHome() {
+    return(
+        <>
+        <SideBar/>
+        <Box 
+        sx={{
+            marginLeft:10,
+            marginTop:5
+           
+        }}
+        >
+            
+            
+        </Box>
+        </>
+    )
+}
