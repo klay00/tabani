7a15184e83b9948fba8bd0a5202171b08373561e
klay00 <aali74432@gmail.com>
2023-06-19 22:38:58 +0300

    Add admin route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AdminHome from './admin/pages/AdminHome';
 import './App.css';
 import pagelist from './components/pagelist';
 import DashUser from './pages/dash_user';
@@ -30,6 +31,7 @@ function App() {
             <Route path='/login' element={<LogIn />} />
             <Route path='/signup' element={<SignUp />} />
             <Route path='/dash_user' element={<DashUser/>} />
+            <Route path='/admin_dash' element={<AdminHome/>} />
             <Route path='*' element={<NoPage/>} />
 
           </Route>
