5f88ebb93d749e2302637c12f6f8ff7e12b11da6
klay00 <aali74432@gmail.com>
2023-06-28 19:33:07 +0300

    add images

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,7 +1,9 @@
-import { Grid } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import React from "react";
 import NavBar from "../components/navbar";
+import '../App.css';
+import { Button, ThemeProvider } from "@mui/material";
+import theme from "../tools/theem";
 
 export default function BlogPage() {
     return (
@@ -28,25 +30,39 @@ export default function BlogPage() {
                         lg: 'row',
                     }}
                     justifyContent="space-between"
+                    marginTop={10}
+                    height={'75vh'}
+                    spacing={10}
                 >
                     <Box
-                    flex={1}
+                        flex={1}
                     >
-                        <h1 
-                        style={{fontSize:45}}
-                        
-                        >Welcome  to <span style={{color:'#FFA800'}}>tabani</span>
-                         when you can get   
-                           
-                        your new best friend</h1>
-                        
+                        <h1
+                            style={{ fontSize: 45, fontWeight: 'bold' }}
+
+                        >
+                            Welcome  to
+                            <span style={{ color: '#FFA800', marginRight: 10, marginLeft: 10 }}>tabani</span>
+                            when you can get
+                            <div className="pet-title-img"></div>
+                            your new best friend</h1>
+                        <p>Choose a pet from among the animal classes and contact the owner to reach an adoption agreement</p>
+                        <ThemeProvider theme={theme}>
+                            <Button variant="contained" sx={{ width: 250, height: 50 }}>GET STARTED</Button>
+                        </ThemeProvider>
                     </Box>
 
-            
+
 
                     <Box
-                    flex={1}
-                    >dddeeeeeee</Box>
+                        flex={1}
+                        sx={{ position: 'relative' }}
+                    >
+                        <div className="main-images-title first-img"></div>
+                        <div className="main-images-title secndy-img"></div>
+                        <div className="main-images-title therrd-img"></div>
+                        <div className="main-images-title fiefth-img"></div>
+                    </Box>
                 </Stack>
             </Box>
         </>
