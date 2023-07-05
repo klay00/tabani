d0d86b2444279856d40ec6e9696367b77c69c7b4
klay00 <aali74432@gmail.com>
2023-07-05 14:52:46 +0300

    fix respoinsiv card

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,88 +1,81 @@
-import * as React from 'react';
+import React from 'react';
 import AspectRatio from '@mui/joy/AspectRatio';
 import Card from '@mui/joy/Card';
 import CardOverflow from '@mui/joy/CardOverflow';
 import Divider from '@mui/joy/Divider';
 import Typography from '@mui/joy/Typography';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { Avatar } from '@mui/joy';
-import { Stack } from '@mui/system';
+import { Stack, Grid } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-
-
-export default function OverflowCard({ petname, userfullName, user, petImage, userLocation, petState, pet, userImage }) {
+export default function OverflowCard({
+  petname,
+  userfullName,
+  user,
+  petImage,
+  userLocation,
+  petState,
+  pet,
+  userImage,
+}) {
   return (
-    <Card className={'card-resp'} variant="outlined" sx={{
-      width: {
-        sm: 160,
-        md: 230, // For medium screens (md)
-        lg: 260, // For large screens (lg)
-      },
-      maxHeight: {
-        sm: "36vh",
-        md: "45vh",
-        lg: "42vh"
-      },
-      overflow: 'hidden',
-      padding: 1,
-      border:'10px solid white'
-    }}>
-
-      <CardOverflow fullWidth >
-        <AspectRatio ratio="1.5" >
-          <Link to={'../petpage'} state={pet}>
-            <img
-              src={petImage}
-              loading="lazy"
-              alt=""
-            />
-          </Link>
-        </AspectRatio>
-      </CardOverflow>
-      <Typography level="h2" sx={{ fontSize: 'md', mt: 1 }}>
-        <Link to={'../petpage'} state={pet}>
-          {[petname]}
-        </Link>
-
-
-      </Typography>
-      <Typography level="body2" sx={{ mt: 0.5, mb: 1 }}>
-        <Link to={'../profile'} state={user}>
-          <Stack
-            direction={{ xs: 'row', sm: 'row' }}
-            spacing="2"
-            alignItems="center"
-          >
-            <Avatar sx={{ marginRight: '10px', borderRadius: '100%' }} src={userImage}></Avatar>
-            {userfullName}
-          </Stack>
-        </Link>
-      </Typography>
-      <Divider />
-      <CardOverflow
-        variant="soft"
+    <Grid item xs={12} sm={6} md={4} lg={3} width={{xs:'100%',lg:'24%',md:'32%',sm:'49%'}} height={'fit-content'} >
+      <Card
+        variant="outlined"
         sx={{
-          display: 'flex',
-          gap: 1.5,
-          py: 1,
-          px: 'var(--Card-padding)',
-          bgcolor: 'background.level1',
+          width: '100%',
+          padding: "15px",
+          border: '10px solid white',
+          aspectRatio:'auto',
+          gap:'3px',
+          
         }}
+       padding={0}
       >
-        <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
-          <Stack
-            direction={{ xs: 'row', sm: 'row' }}
-            spacing="2"
-            alignItems="center">
-            <LocationOnIcon></LocationOnIcon> {userLocation}
-          </Stack>
+        <CardOverflow fullWidth>
+          <AspectRatio ratio="1.5">
+            <Link to={'../petpage'} state={pet}>
+              <img src={petImage} loading="lazy" alt="" />
+            </Link>
+          </AspectRatio>
+        </CardOverflow>
+        <Typography level="h2" sx={{ fontSize: 'md', mt: 1 }}>
+          <Link to={'../petpage'} state={pet}>
+            {petname}
+          </Link>
         </Typography>
-        <Divider orientation="vertical" />
-        <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
-          {petState}
+        <Typography level="body2" >
+          <Link to={'../profile'} state={user}>
+            <Stack direction={{ xs: 'row', sm: 'row' }} spacing={2} alignItems="center">
+              <Avatar sx={{ marginRight: '10px', borderRadius: '100%' }} src={userImage}></Avatar>
+              {userfullName}
+            </Stack>
+          </Link>
         </Typography>
-      </CardOverflow>
-    </Card>
+        <Divider />
+        <CardOverflow
+          variant="soft"
+          sx={{
+            display: 'flex',
+            gap: 1.5,
+            py: 1,
+            px: 'var(--Card-padding)',
+            bgcolor: 'background.level1',
+          }}
+        >
+          <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
+            <Stack direction={{ xs: 'row', sm: 'row' }} spacing={2} alignItems="center">
+              <LocationOnIcon />
+              {userLocation}
+            </Stack>
+          </Typography>
+          <Divider orientation="vertical" />
+          <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
+            {petState}
+          </Typography>
+        </CardOverflow>
+      </Card>
+    </Grid>
   );
 }
